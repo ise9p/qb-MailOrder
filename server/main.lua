d15eff8197dc63bb9aa14bb18a1473c26cf4a257
se9p <71358264+ise9p@users.noreply.github.com>
2025-03-27 12:06:52 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,26 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 local Mailboxes = {}
 
+local function SendWebhook(title, description, color)
+    if not Config.Webhooks.enabled or Config.Webhooks.url == "" then return end
+    
+    local embed = {
+        {
+            ["title"] = title or Config.Webhooks.title,
+            ["description"] = description,
+            ["color"] = color or Config.Webhooks.color,
+            ["footer"] = {
+                ["text"] = Config.Webhooks.footer,
+            },
+            ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+        }
+    }
+    
+    PerformHttpRequest(Config.Webhooks.url, function(err, text, headers) end, 'POST', json.encode({
+        username = 'Mail System',
+        embeds = embed
+    }), { ['Content-Type'] = 'application/json' })
+end
 
 RegisterNetEvent('QBCore:Server:OnPlayerLoaded')
 AddEventHandler('QBCore:Server:OnPlayerLoaded', function()
@@ -18,16 +38,27 @@ QBCore.Functions.CreateCallback('qb-MailOrder:server:getMailID', function(source
     cb(mailid)
 end)
 
-
-
 RegisterNetEvent('qb-MailOrder:server:checkAnyMailbox')
 AddEventHandler('qb-MailOrder:server:checkAnyMailbox', function(mailid)
     local src = source
+    local Player = QBCore.Functions.GetPlayer(src)
+    
     if Config.inventory == "ox" then
         exports.ox_inventory:RegisterStash("mailbox_"..mailid, "Mail Box #"..mailid, Config.Storage.slots, Config.Storage.weight)
     elseif Config.inventory == "qb" then
         -- QB Inventory doesn't need stash registration
     end
+    
+    SendWebhook(
+        "Mailbox Access",
+        string.format("Player: %s\nMail ID: %s\nCharacter: %s %s", 
+            GetPlayerName(src),
+            mailid,
+            Player.PlayerData.charinfo.firstname,
+            Player.PlayerData.charinfo.lastname
+        )
+    )
+    
     TriggerClientEvent('qb-MailOrder:client:openStash', src, mailid)
 end)
 
