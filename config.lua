03450385008d706a174b2d981521676f32d610fa
se9p <71358264+ise9p@users.noreply.github.com>
2025-03-27 12:06:40 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 Config = {}
 
-Config.inventory = "qb" -- "qb" for qb-inventory or "ox" for ox_inventory
-Config.input = "qb"     -- "qb" for qb-input or "ox" for ox_lib input
+Config.inventory = "ox" -- "qb" for qb-inventory or "ox" for ox_inventory
+Config.input = "ox"     -- "qb" for qb-input or "ox" for ox_lib input
 Config.target = "qb"    -- "qb" for qb-target or "ox" for ox_target
-Config.Notify = "qb"    -- "qb" for QBCore.Functions.Notify or "ox" for lib.notify
+Config.Notify = "ox"    -- "qb" for QBCore.Functions.Notify or "ox" for lib.notify
 
 Config.BotLocation = vector4(-232.09, -915.49, 31.31, 337.52)
 
@@ -17,4 +17,36 @@ Config.Blip = {
 Config.Storage = {
     slots = 5,    
     weight = 50000 
+}
+
+Config.TimeRestriction = {
+    enabled = true,
+    closeTime = 23, -- 11 PM
+    openTime = 6    -- 6 AM
+}
+
+Config.Ped = {
+    model = `a_m_y_bevhills_01`,
+    animation = {
+        dict = "missfam4",
+        name = "base",
+        flag = 49
+    },
+    scenario = nil, -- if you want scenario instead of animation
+    props = {
+        {
+            model = `p_amb_clipboard_01`,
+            bone = 36029,
+            coords = vec3(0.16, 0.08, 0.1),
+            rotation = vec3(-130.0, -50.0, 0.0)
+        }
+    }
+}
+
+Config.Webhooks = {
+    enabled = true,
+    url = "", -- Your Discord webhook URL
+    color = 3447003, -- Blue color
+    footer = "Mail System Logs",
+    title = "Mail System"
 }
